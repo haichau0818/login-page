6d585edf7215cfa94df3c4452a619ab6a786e0b3
kane chau <haichau0818@gmail.com>
2023-11-01 13:12:39 +0700

    push

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const container= document.getElementById('container');
+const regiterBtn=document.getElementById('register');
+const loginBtn=document.getElementById('login');
+
+
+regiterBtn.addEventListener(
+    'click',()=>{container.classList.add("active");
+})
+
+loginBtn.addEventListener(
+    'click',()=>{container.classList.remove("active");
+})
